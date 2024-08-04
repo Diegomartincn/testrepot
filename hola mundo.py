ed5f78621b321f6973df0e8fbea26678b90ec0a6
Diegomartincn <diegomartincn@gmail.com>
2024-08-04 20:08:22 +0200

    Add files via upload

---
+++ hola mundo.py
@@ -0,0 +1,16 @@
+# comentariio de programa hola mundo
+print("hola mundo")
+
+'''
+comentario a 
+varias lineas 
+hola mundo
+'''
+print("hola_mundo)")
+
+# comentario comando type
+print(type("hello_word"))
+print(type(1))
+print(type(1.5))
+print(type(True))
+print(type(1 + 1j))
